3ddd4b38d4ac0341a0a026991824169f126b6ab3
BoAndArrows <bocheung15@gmail.com>
2025-01-21 17:34:22 -0500

    starting

---
+++ PerciseClick.py
@@ -0,0 +1,10 @@
+import pyautogui
+import appJar
+import tkinter
+
+app = appJar.gui("Percise Clicker")
+
+root = appJar.Tk()
+Default_Height = root.winfo_screenwidth()/10
+Default_Width = root.winfo_screenheight()/5
+
